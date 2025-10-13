7087502cba3c0da68ff2fbcc4af80cc16dfa1c29
Shreyas Kamath <shreyass29122001@gmail.com>
2025-10-13 17:51:31 +0530

    changed string + int IPs to boost::asio ip address structs

--- source/include/Peer.hpp
+++ source/include/Peer.hpp
@@ -8,16 +8,30 @@
 
 #include <Bencode.hpp>
 
+#include <boost/asio/ip/address.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
 class Peer {
 public:
-    Peer(const std::string& ip, uint16_t port) : ip_(std::move(ip)), port_(port) {}
+    Peer(const boost::asio::ip::address& address, uint16_t port) : endpoint_(address, port) {}
 
-    const std::string& ip() const { return ip_; }
-    uint16_t port() const { return port_; }
+    const boost::asio::ip::tcp::endpoint& endpoint() const { return endpoint_; }
+
+    std::string ip() const { return endpoint_.address().to_string(); }
+    uint16_t port() const { return endpoint_.port(); }
+
+    bool operator==(const Peer& other) const {
+        return this->ip() == other.ip() && this->port() == other.port();
+    }
 
 private:
-    std::string ip_;
-    uint16_t port_;
+    boost::asio::ip::tcp::endpoint endpoint_;
+};
+
+struct PeerHash {
+    size_t operator()(const Peer& p) const noexcept {
+        return std::hash<std::string>()(p.ip()) ^ (std::hash<uint16_t>()(p.port()) << 1);
+    }
 };
 
 std::vector<Peer> parse_compact_peers(const BEncodeValue& peers_blob);
